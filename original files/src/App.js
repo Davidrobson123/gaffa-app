7ca5c2291a977f4592bcf21ab3a8a622bdc2ec22
Andy Terry <andyterrydesign@gmail.com>
2019-01-28 20:46:17 +0000

    tried adding tracked files

---
+++ original files/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <Button bsStyle="success">Bootstrap Button</Button>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
